e6957bd2638dbbf9b3e4ded30cd1696f950f42d5
Felipe Falcão <felpssalvatore42@gmail.com>
2021-05-26 19:03:27 -0300

    Programa para calculo de porcentagem

---
+++ Calc/main..py
@@ -0,0 +1,6 @@
+porcento = float(input('Digite o porcento: '))
+valor = float(input('Digite o valor: '))
+
+result = (valor /100) * porcento
+
+print(f'\n{porcento:.0f}% de {valor:.0f} = {result:.0f}')
