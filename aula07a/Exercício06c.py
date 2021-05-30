3c9132511333880fb59e988131ff2c7c50979657
Felipe Falcão <felpssalvatore42@gmail.com>
2021-05-30 14:36:35 -0300

    programa para calcular a porcentagem de um valor

--- aula07a/Exercício06c.py
+++
@@ -1,6 +0,0 @@
-nota1 = float(input('Digite a primeira nota do aluno: '))
-nota2 = float(input('Digite a segunda nota do aluno: '))
-
-media = (nota1 + nota2) /2
-
-print(f'A média desse aluno foi {media}')
---
+++ aula07a/Exercício07.py
@@ -0,0 +1,10 @@
+# Programa que ler duas notas de um aluno,
+# calcula e mostra a média aritmética.
+
+nota1 = float(input('Primeira nota do aluno: '))
+nota2 = float(input('Segunda nota do aluno: '))
+# Entrada de dados
+
+media = (nota1 + nota2) / 2  # Operação (calculo)
+
+print(f'A média entre {nota1} e {nota2} é igual a {media:.1f}') # Saída
