a94b9f0ed7b69fc2e7e999d880f71aa870243dee
Felipe Falcão <felpssalvatore42@gmail.com>
2021-06-01 13:32:52 -0300

    Ler a largura e altura e diz a quantidade de tinta que precisa para pintar a área

--- aula07a/Exercício10g.py
+++ aula07a/Exercício10g.py
@@ -1,1 +1,11 @@
-# 11
+# Programa que mede a largura e a altura de uma parede
+# e diz quanto de tinta precisa para pintar.
+
+l = float(input('Largura da parade: '))
+a = float(input('Altura da parede: '))
+
+area = l * a
+litros =  area /2
+
+print(f'Sua parede tem a dimensão de {l}x{a} e sua área é de {area}m²')
+print(f'Para pintar essa parede, você precisará de {litros}l de tinta')
