64f2e9e05d76f470462cd5f805705850de92a16a
Felipe Falcão <felpssalvatore42@gmail.com>
2021-06-03 20:04:27 -0300

    Porcentagem de um preço

--- aula07a/Exercício12.py
+++ aula07a/Exercício12.py
@@ -1,7 +1,10 @@
-preco = float(input('Digite o preço do produto: '))
-desconto = int(input('Qual o desconto do produto ? '))
+# Algoritmo que ler o preço de um produto e mostra seu
+# novo preço, com tanto porcento de desconto e se novo preço.
 
-novoPreco = preco-(preco / 100 * desconto)
-descont = (preco / 100 * desconto)
+p = float(input('Qual é o preço do produto? R$'))
+porcento = int(input('Qual o desconto do produto? %'))
 
-print(f'O novo preço do produto é {novoPreco} com desconto de {desconto}% ({descont})')
+nP = p-(p / 100 * porcento)
+d = (p / 100 * porcento)
+
+print(f'O produto que custava {p}, na promoção com desconto de {porcento}% vai custar R${nP:.2f}')
