023dab24c798e83328f984fed781eba19124dc2b
Felipe Falcão <felpssalvatore42@gmail.com>
2021-06-12 20:58:23 -0300

    Calcula o aumento de tantos porcentos

--- aula07a/Exercício13.py
+++ aula07a/Exercício13.py
@@ -1,7 +1,10 @@
-salario = float(input('Informe o salário.: '))
-p = int(input('Qual o porcento de aumento: '))
+# Programa que vai ler o salário inicial de um funcionário e calcular o aumento de tantos %
 
-nS = salario+(salario /100 * p)
+salario = float(input('Informe o salário? R$ '))
+p = int(input('Qual a taxa de aumento? '))
+
+ns = salario+(salario * p /100)
 aumento = (salario /100 * p)
 
-print(f'Novo salário é {nS} com {p}% ({aumento}) de aumento')
+print(f'Um funcionário que ganha R${salario}, com {p}% de aumento, passa a receber R${ns:.2f}')
+print(f'Com aumento de R${aumento:.2f}')
