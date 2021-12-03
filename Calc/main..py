9199fb2a3a06608f118338fb3a81da07cc8a492c
Felipe Falcão <81134140+felipe39@users.noreply.github.com>
2021-12-02 23:32:49 -0300

    Delete Calc directory

--- Calc/main..py
+++
@@ -1,7 +0,0 @@
-porcento = float(input('Digite o número de percento: '))
-valor = int(input('Digite o valor a ser descontado: '))
-
-result = (valor /100) * porcento
-
-print(f'\n{porcento:.1f}% de {valor} = {result:.2f}')
-
